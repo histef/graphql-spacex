53322a47a3fca903724ef5e374385294b12bc969
Stefanie Chin <hi.stef.c@gmail.com>
2019-12-03 21:59:14 -0800

    added classnames for launch success/failure styling

--- client/src/components/LaunchListItem.js
+++ client/src/components/LaunchListItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Moment from 'react-moment';
+import classNames from 'classnames';
 
 export default function LaunchListItem({launch: {flight_number, mission_name, launch_date_local, launch_success}}) {
   return (
     <div className='card card-body' style={{margin: '12px'}}>
       <div className='row'>
         <div className=" col-md-9">
-          <h3>Mission: {mission_name}</h3>
+          <h3>Mission: <span className={classNames({'text-success': launch_success, 'text-danger': !launch_success})}>{mission_name}</span></h3>
           <p className="card-subtitle mb-2 text-muted">
             Date: <Moment format="L">{launch_date_local}
             </Moment>
